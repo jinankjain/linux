1a4b5e19aa388c780517774886a563d4537e082f
Jinank Jain <jinankjain@microsoft.com>
2024-04-18 11:14:37 +0530

    x86/hyperv: Don't enable HV_APIC on SEV-SNP guest
    SEV-SNP guests cannot use the HV_APIC enlightment, thus disable it.
    
    Signed-off-by: Jinank Jain <jinankjain@microsoft.com>

--- arch/x86/hyperv/hv_init.c
+++ arch/x86/hyperv/hv_init.c
@@ -601,7 +601,8 @@ skip_hypercall_pg_init:
 	old_setup_percpu_clockev = x86_init.timers.setup_percpu_clockev;
 	x86_init.timers.setup_percpu_clockev = hv_stimer_setup_percpu_clockev;
 
-	hv_apic_init();
+	if (!cc_platform_has(CC_ATTR_GUEST_SEV_SNP))
+		hv_apic_init();
 
 	x86_init.pci.arch_init = hv_pci_init;
 
